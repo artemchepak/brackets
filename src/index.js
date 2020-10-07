8d0b4dd58d2bae36f20a5c2b1e90174c192534f0
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2020-10-07 22:15:25 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strInput = str;
+  let arrayNumber = 0;
+  let re = '';
+  let n = 0;
+
+  function find(arrayNumber){
+    let currentConfig = bracketsConfig[arrayNumber];
+    re = currentConfig[0] + currentConfig[1];
+    return strInput.indexOf(currentConfig[0] + currentConfig[1]);
+  }
+
+  while(n < str.length){
+    while(arrayNumber < bracketsConfig.length){
+      while (find(arrayNumber) !== -1){
+        strInput = strInput.replace(re, '')
+      }
+      arrayNumber++;
+    }
+    n++;
+    arrayNumber = 0;
+  }
+
+  return strInput === '';
 }
